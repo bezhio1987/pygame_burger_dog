b95b0df21ef014121325acc7ccabf1eedc94be8f
sereno <sereno.project@gmail.com>
2023-04-18 22:03:30 +0330

    finalizing the game

--- game_4_burgur-dog.py
+++ game_4_burgur-dog.py
@@ -11,7 +11,6 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Burger Dog")
 
 
-
 # define a clock to slow down the while loop and make sure it runs at the same speed on every single computer (FPS = frame per second)
 FPS  = 60
 clock = pygame.time.Clock()
@@ -100,6 +99,7 @@ burger_rect.topleft = (random.randint(0, WINDOW_WIDTH-32), -100)
 
 
 #the main game loop
+pygame.mixer.music.play()
 running = True
 while running:
     #lopp through a list of ecents 
@@ -107,8 +107,91 @@ while running:
         if event.type == pygame.QUIT:
            running = False 
            
+           
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and player_rect.left > 0:
+        player_rect.x -= player_velocity
+        player_image = player_image_left
+    if keys[pygame.K_RIGHT] and player_rect.right < WINDOW_WIDTH:
+        player_rect.x += player_velocity
+        player_image = player_image_right
+    if keys[pygame.K_UP] and player_rect.top > 100:
+        player_rect.y -= player_velocity
+    if keys[pygame.K_DOWN] and player_rect.bottom < WINDOW_HEIGHT:
+        player_rect.y += player_velocity
+        
+    #engage boost
+    if keys[pygame.K_SPACE] and boost_level >0:
+        player_velocity = PLAYER_BOOST_VELOCITY
+        boost_level -= 1
+    else:
+        boost_level = PLAYER_NORMAL_VELOCITY
 
+    #move the burger and update the burger points
+    burger_rect.y += burger_velocity
+    
+    #the faster the burger moves the more points it  worths
+    burger_points = int(burger_velocity * (WINDOW_HEIGHT - burger_rect.y + 100))
+    
+    # player missed the burger
+    if burger_rect.y > WINDOW_HEIGHT:
+        player_lives -= 1
+        miss_sound.play()
+        burger_rect.topleft = (random.randint(0, WINDOW_WIDTH-32), -BUFFER_DISTANCE)
+        burger_velocity = STARTER_BURGER_VELOCITY
+        
+        player_rect.centerx = WINDOW_WIDTH//2
+        player_rect.bottom =WINDOW_HEIGHT
+        boost_level = STARTING_BOOST_LEVEL
+        
+    if player_rect.colliderect(burger_rect):
+        score += burger_points
+        burger_eaten += 1
+        bark_sound.play()
+        
+        #relocate the player
+        burger_rect.topleft = (random.randint(0, WINDOW_WIDTH-32), -BUFFER_DISTANCE)
+        burger_velocity += BURGER_ACCELERATION
+        
+        boost_level += 25
+        if boost_level >STARTING_BOOST_LEVEL:
+            boost_level = STARTING_BOOST_LEVEL
+            
+        
+        
+    # fill the surface
     display_surface.fill(BLACK)
+    
+    #update HUD
+    score_text = font.render("Score: "+ str(score), True, ORANGE)
+    points_text = font.render("Points: "+str(burger_points), True, ORANGE)
+    eaten_text = font.render("Burgers eaten: : "+str(burger_eaten), True, ORANGE)
+    boost_text = font.render("Boost: "+str(boost_level), True, ORANGE)
+    lives_text = font.render("Lives: "+str(player_lives), True, ORANGE)
+    
+    if player_lives == 0:
+            score_text = font.render("Score: "+ str(score), True, ORANGE)
+            display_surface.blit(game_over_text, game_over_rect)
+            display_surface.blit(continue_text, continue_rect)
+            pygame.display.update()
+            
+            #pause the game until player presses a key and reset the game
+            pygame.mixer.music.stop()
+            is_paused = True
+            while is_paused:
+                for event in pygame.event.get():
+                    if event.type == pygame.KEYDOWN:
+                        score = 0
+                        player_lives = PLAYER_STARTING_LIVES
+                        burger_eaten = 0
+                        boost_level = STARTING_BOOST_LEVEL
+                        burger_velocity = STARTER_BURGER_VELOCITY
+                        pygame.mixer.music.play(-1, 0.0)
+                        is_paused = False
+                    #the player wants to quit
+                    if event.type == pygame.QUIT:
+                        is_paused = False
+                        running = False 
     
     #blit HUD
     display_surface.blit(score_text, score_rect)
